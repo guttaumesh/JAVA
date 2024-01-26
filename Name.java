50fc1f13cc2aa90dc955492d432ce67b60551a60
guttaumesh <119066890+guttaumesh@users.noreply.github.com>
2024-01-26 22:28:43 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,12 @@
+//import java.io.*;
+import java.util.Scanner;
+class Name
+{
+public static void main (String a[])
+{
+    Scanner s=new Scanner(System.in);
+    String name=s.nextLine();
+System.out.println("name"+ name);
+
+}
+}
---
+++ multiplicationtable.java
@@ -0,0 +1,18 @@
+/*Display a Multiplication Table: Write a Java program that displays the multiplication 
+table of a given number. Ask the user for the number and print the table from 1 to 10.*/
+
+import java.util.*;
+class add
+ {
+    public static void main(String[] args)
+    {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter any integer to print multiplication table:\n");
+	int a=sc.nextInt();
+	for(int i=1;i<=10;i++)
+	{
+	System.out.println(a+"*"+i+"="+(a*i)+"\n");
+	}
+	sc.close();
+    }
+ }
